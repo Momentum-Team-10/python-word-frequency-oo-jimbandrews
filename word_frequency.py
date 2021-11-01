cb9ebfbf8e5fea927100f2a8c2a18263126c41e7
James Andrews <jbandrew14@gmail.com>
2021-11-01 19:28:51 -0400

    implemented word_frequency using OOP, also limited number of words printed to 25

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
@@ -7,19 +9,21 @@ STOP_WORDS = [
 
 class FileReader:
     def __init__(self, filename):
-        pass
+        self.filename = filename
 
     def read_contents(self):
         """
         This should read all the contents of the file
         and return them as one string.
         """
-        raise NotImplementedError("FileReader.read_contents")
+        with open(self.filename) as opened_file:
+            file_string = opened_file.read()
+        return file_string
 
 
 class WordList:
     def __init__(self, text):
-        pass
+        self.text = text
 
     def extract_words(self):
         """
@@ -27,14 +31,29 @@ class WordList:
         is responsible for lowercasing all words and stripping
         them of punctuation.
         """
-        raise NotImplementedError("WordList.extract_words")
+        zero_punctuation = self.text.lower()
+        punctuation = string.punctuation + "’" + "”" + "“"
+        for char in punctuation:
+            if char in zero_punctuation:
+                zero_punctuation = zero_punctuation.replace(char, '')
+        replace_whitespace = ["\n", "—", "  "]
+        for char in replace_whitespace:
+            if char in zero_punctuation:
+                zero_punctuation = zero_punctuation.replace(char, " ")
+        self.text = zero_punctuation.split(' ')
 
+# do i want to keep sel.text as string or change to a list?
     def remove_stop_words(self):
         """
         Removes all stop words from our word list. Expected to
         be run after extract_words.
         """
-        raise NotImplementedError("WordList.remove_stop_words")
+        if type(self.text) == str:
+            return print("Please run extract_words first.")
+        words_copy = self.text.copy()
+        for word in words_copy:
+            if word in STOP_WORDS:
+                self.text.remove(word)
 
     def get_freqs(self):
         """
@@ -43,12 +62,20 @@ class WordList:
         extract_words and remove_stop_words. The data structure
         could be a dictionary or another type of object.
         """
-        raise NotImplementedError("WordList.get_freqs")
+        word_count = {}
+        for word in self.text:
+            if word == '':
+                continue
+            elif word in word_count.keys():
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+        return word_count
 
 
 class FreqPrinter:
     def __init__(self, freqs):
-        pass
+        self.freqs = freqs
 
     def print_freqs(self):
         """
@@ -67,7 +94,30 @@ class FreqPrinter:
        rights | 6    ******
         right | 6    ******
         """
-        raise NotImplementedError("FreqPrinter.print_freqs")
+        max_length = max([len(word) for word in self.freqs])
+        max_freq = max(self.freqs.values())
+        frequency_groups = []
+        while max_freq > 0:
+            frequency_groups.append([])
+            max_freq -= 1
+        for word in self.freqs:
+            frequency_groups[0-self.freqs[word]].append(word)
+        list_count = 0
+        for group in frequency_groups:
+            group.sort()
+            for word in group:
+                freq = self.freqs[word]
+                spaces = (max_length - len(word)) * " "
+                if freq < 10:
+                    stars = "  " + freq * "*"
+                else:
+                    stars = " " + freq * "*"
+                print(f" {spaces}{word} | {freq}{stars}")
+                list_count += 1
+                if list_count == 25:
+                    break
+            if list_count == 25:
+                break
 
 
 if __name__ == "__main__":
